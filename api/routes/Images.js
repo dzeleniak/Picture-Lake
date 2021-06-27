8ba2c3edae64904a31e625f131e5e1577fd6a23d
David Zeleniak <davidzeleniak@davids-mbp.lan>
2021-06-27 13:38:31 -0700

    Created upload component for pictures

--- api/routes/Images.js
+++ api/routes/Images.js
@@ -5,14 +5,6 @@ AWS.config.update({region: 'us-west-2'});
 
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
-s3.listBuckets((err,data) => {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log(data.Buckets);
-    }
-});
-
 const router = express.Router();
 
 // TODO: create AWS object
