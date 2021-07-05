70fe0f4844e2b79142af32ebdfbe2f45a2ec1e13
David Zeleniak <davidzeleniak@davids-mbp.lan>
2021-07-05 15:29:25 -0700

    Deployed dev environments and modified scripts

--- api/Database/db.js
+++ api/Database/db.js
@@ -6,12 +6,12 @@ const connectionString = process.env.DATABASE_URL;
 const db = new Sequelize(connectionString, {
     dialect: 'postgres',
     protocol: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
-        }
-    }
+    // dialectOptions: {
+    //     ssl: {
+    //         require: true,
+    //         rejectUnauthorized: false,
+    //     }
+    // }
 });
 
 db.authenticate()
