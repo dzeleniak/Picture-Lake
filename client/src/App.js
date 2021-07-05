b4ad4a37eabb7cd77045636fa9e6d110034e7a39
David Zeleniak <davidzeleniak@davids-mbp.lan>
2021-07-05 15:09:27 -0700

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,27 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Card from './components/Card/Card';
 import Upload from './components/Upload/Upload';
+import axios from 'axios';
+import { useState, useEffect } from 'react'
+
 function App() {
+
+  const [images, setImages] = useState(null);
+
+  const apiUrl = process.env.REACT_APP_API_URL;
+  
+  useEffect(()=>{
+    axios.get(apiUrl + "images")
+      .then(res =>{
+        setImages(res.data.body);
+      });
+  }, [])
+
   return (
     <div className="App">
       <Navbar />
       <Upload />
+      {images && images.map(item => <Card key={item.id} name={item.name} url={item.url} description={item.description} user={item.username}/>)}
     </div>
   );
 }
--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Card.css';
 
-export default function Card({imageUrl, description, user}) {
+export default function Card({name, url, description, user}) {
     return (
         <div className="card-container">
-            <img src={imageUrl} alt="" className="card-image"/>
+            <img src={url} alt="" className="card-image"/>
             <div className="card-text">
+                <h3>{name}</h3>
                 <h3>{description}</h3>
                 <p>{user}</p>
             </div>
